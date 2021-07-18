e3c3267f8a58d3c1c20b985ba84c7bba76089bc1
HR Williams <halrubinwilliams@gmail.com>
2021-07-18 15:23:52 -0700

    Add passing test for dollars
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>

--- __tests__/coinCounter.test.js
+++ __tests__/coinCounter.test.js
@@ -5,4 +5,8 @@ describe('coinCounter', () => {
   test('should return 1 for the dollar amount if 1.00 is entered', () => {
     expect(coinCounter(1.00)).toEqual("dollar bills = 1")
   });
+  test('should return any number for the dollar amount if any number of dollars is entered', () => {
+    expect(coinCounter(4.00)).toEqual("dollar bills = 4")
+    expect(coinCounter(22.00)).toEqual("dollar bills = 22")
+  });
 });
